37e673e33f0618adf9b98cd86d7ac0ba6cb56b9f
HungCuong <cuongnguyen0834@gmail.com>
2023-10-30 13:53:22 +0700

    thuongtich

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b) {
+	return a * b;
+}
+float thuong(int a, int b) {
+	if (b == 0)
+		cout << "\n B sai vui long nhap lai";
+	else
+	{
+		return a / b;
+	}
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
